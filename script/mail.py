aa5400cd5c8c1fd464841dccc8944a01a84d9a71
flyq <flyq951@gmail.com>
2019-03-03 14:43:53 +0800

    update

--- script/mail.py
+++ script/mail.py
@@ -4,11 +4,11 @@ from email.mime.text import MIMEText
 #163邮箱服务器地址
 mail_host = 'smtp.163.com'  
 #163用户名
-mail_user = '18810687830'  
+mail_user = 'xxxxx@163.com'  
 #密码(部分邮箱为授权码) 
-mail_pass = '18810687830a'   
+mail_pass = 'xxxxx'   
 #邮件发送方邮箱地址
-sender = '18810687830@163.com'  
+sender = 'xxx@163.com'  
 #邮件接受方邮箱地址，注意需要[]包裹，这意味着你可以写多个邮件地址群发
 receivers = ['2743275429@qq.com']  
 
