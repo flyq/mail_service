8428bb59c74dfae5a856b98a56804741d0b00ccc
flyq <flyq951@gmail.com>
2019-03-04 09:46:36 +0800

    update

--- script/mail.py
+++ script/mail.py
@@ -1,39 +1,43 @@
 import smtplib
+import sys
 from email.mime.text import MIMEText
-#设置服务器所需信息
-#163邮箱服务器地址
-mail_host = 'smtp.163.com'  
-#163用户名
-mail_user = 'xxxxx@163.com'  
-#密码(部分邮箱为授权码) 
-mail_pass = 'xxxxx'   
-#邮件发送方邮箱地址
-sender = 'xxx@163.com'  
-#邮件接受方邮箱地址，注意需要[]包裹，这意味着你可以写多个邮件地址群发
-receivers = ['2743275429@qq.com']  
 
-#设置email信息
-#邮件内容设置
-message = MIMEText('content','plain','utf-8')
-#邮件主题       
-message['Subject'] = 'title' 
-#发送方信息
-message['From'] = sender 
-#接受方信息     
-message['To'] = receivers[0]  
 
-#登录并发送邮件
-try:
-    smtpObj = smtplib.SMTP() 
-    #连接到服务器
-    smtpObj.connect(mail_host,25)
-    #登录到服务器
-    smtpObj.login(mail_user,mail_pass) 
-    #发送
-    smtpObj.sendmail(
-        sender,receivers,message.as_string()) 
-    #退出
-    smtpObj.quit() 
-    print('success')
-except smtplib.SMTPException as e:
-    print('error',e) #打印错误
+def sendMail(to, content):
+    mail_host = 'smtp.163.com'  
+    mail_user = '18810687830@163.com'  
+    mail_pass = 'sb18810687830sb'   
+    sender = '18810687830@163.com'  
+    receivers = to
+
+    message = MIMEText(content,'plain','utf-8')
+    message['Subject'] = 'Buy Key From Smart Signature' 
+    message['From'] = sender 
+  
+    message['To'] = to
+
+    #登录并发送邮件
+    try:
+        smtpObj = smtplib.SMTP() 
+        #连接到服务器
+        smtpObj.connect(mail_host,25)
+        #登录到服务器
+        smtpObj.login(mail_user,mail_pass) 
+        #发送
+        smtpObj.sendmail(
+            sender,receivers,message.as_string()) 
+        #退出
+        smtpObj.quit() 
+        print('success')
+    except smtplib.SMTPException as e:
+        print('error',e) #打印错误
+
+def main():
+    if len(sys.argv) != 3:
+        print("Error: invalid parameters")
+    else:
+        print('发送Key %s 致 %s 邮箱：' % (sys.argv[1],sys.argv[2])
+        sendMail(argv[1], argv[2])
+
+if __name__ == "__main__":
+    main()
